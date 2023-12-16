e48240f479356e103ac12397a74f2839a2cedfaf
darkwolfcreek <146655192+darkwolfcreek@users.noreply.github.com>
2023-12-16 09:50:20 -0800

    docs: initial

---
+++ BlazorSearch/Program.cs
@@ -0,0 +1,39 @@
+using BlazorSearch.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<HttpClient>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", new CorsPolicyBuilder()
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .Build());
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors("AllowAll");
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
